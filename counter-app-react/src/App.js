a8be4a67aa2fb951090d03d577363aa996459a67
Adria Tavares <adria.tavares28@gmail.com>
2022-01-22 16:03:32 -0300

    Adicionando reescrevendo o código do counter App em React e adicionando estilização simples

--- counter-app-react/src/App.js
+++ counter-app-react/src/App.js
@@ -1,20 +1,46 @@
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
 import { Buttons } from './components/Buttons';
 
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  padding: 0 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
+const ContainerDiv = styled.div`
+  width: 40rem;
+  padding: 1rem;
+  text-align: center;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: lightblue;
+`
+
+const Autora = styled.p`
+  width: 50%;
+  text-align: center;
+  padding-top: 0.5rem;
+`
+
 function App() {
   return (
-    <div>
-      <h1>Contador de Número</h1>
+    <Container>
+      <ContainerDiv>
+        <h1>Contador de Número</h1>
 
-      <span>0</span>
+        <Buttons/>
 
-      <hr/>
-
-      <Buttons></Buttons>
-
-      <p>Desenvolvido por Ádria Tavares, com base no projeto Counter App - 100 days of JS</p>
-    </div>
+        <Autora>Desenvolvido por Ádria Tavares, com base no projeto Counter App - 100 days of JS</Autora>
+      </ContainerDiv>
+    </Container>
   );
 }
 
--- counter-app-react/src/components/Buttons.js
+++ counter-app-react/src/components/Buttons.js
@@ -1,29 +1,93 @@
 import React from "react";
-import arrowDown from "../images/down-arrow.png"
-import arrowSpinner from "../images/rotated.png"
-import arrowUp from "../images/up-arrow.png"
+import arrowDown from "../images/down-arrow.png";
+import arrowSpinner from "../images/rotated.png";
+import arrowUp from "../images/up-arrow.png";
+import styled from "styled-components";
+
+// Estilização por styled-components
+
+const Button= styled.button`
+    padding: 0.5rem;
+    margin: 0.3rem 1rem;
+    font-size: 1.5rem;
+    border: none;
+    border-radius: 3px;
+    outline: none;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 180px;
+
+
+    img {
+        width: 30px;
+        height: 30px;
+    }
+
+    p {
+        font-weight: bold;
+        margin: 0;
+    }
+`
+
+const ButtonsContainer = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    flex-wrap: wrap;
+`
+
+const CounterNumber = styled.p`
+    font-size: 4rem;
+    text-align: center;
+    margin: 0;
+`
 
 
 export class Buttons extends React.Component {
+    state={
+        counter: 0
+    };
+
+    onClickReset = () => {
+        this.setState({counter: 0});
+    };
+
+    onClickAdd = () => {
+        this.setState({counter: this.state.counter + 1});
+    };
+
+    onClickSubtract = () => {
+        this.setState({counter: this.state.counter - 1});
+    };
+
     render() {
         return(
             <div>
 
-                <button>
-                    <img src={arrowDown} alt="Ícone de flecha apontada para baixo" />
-                    Subtrair
-                </button>
+                <CounterNumber>{this.state.counter}</CounterNumber>
 
-                <button>
-                    <img src={arrowSpinner} alt="Ícone de flecha em rotação" />
-                    Reiniciar
-                </button>
+                <hr/>
 
-                <button>
-                    <img src={arrowUp} alt="Ícone de flecha apontada para cima" />
-                    Somar
-                </button>
+                <ButtonsContainer>
+                    <Button onClick={this.onClickSubtract} >
+                        <img src={arrowDown} alt="Ícone de flecha apontada para baixo"/>
+                        <p>Subtrair</p>
+                    </Button>
+
+                    <Button onClick={this.onClickReset} >
+                        <img src={arrowSpinner} alt="Ícone de flecha em rotação" />
+                        <p>Reiniciar</p>
+                    </Button>
+
+                    <Button onClick={this.onClickAdd}>
+                        <img src={arrowUp} alt="Ícone de flecha apontada para cima" />
+                        <p>Somar</p>
+                    </Button>
+                </ButtonsContainer>
             </div>
-        )
-    }
+        );
+    };
 }
